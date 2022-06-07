c985887cbb9ba9933f45939cd1e5fccdf63cc51f
Kaiming Cui <ckm@nao.cas.cn>
2022-06-08 01:18:00 +0800

    Add: some tests of methods of lightcurve object

--- tests/test_lc_objects.py
+++ tests/test_lc_objects.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 import lightkurve_ext as lkx
 
 def test_lc_collection():
@@ -20,3 +21,21 @@ def test_lc_object():
     lc_list = lc_dir.search_lightcurve(188589164, author=['SPOC', 'TESS-SPOC'])
     lc = lc_list.load()[0]
     assert isinstance(lc, lkx.LightCurve)
+
+    # test lightcurve object and split_by_gap, fill_gaps, and fast_bin methods
+    lc_dir = lkx.LightCurveDirectory('./tests/data/', scan_dir=True)
+    lcc = lc_dir.search_lightcurve(73228647, author='SPOC').load().select_lc_with_author_priority()
+    lc = lcc.stitch().remove_nans()
+    lc_length = len(lc)
+    l = 0
+    for lc in lc.split_by_gap():
+        print(np.diff(lc.time.value).max())
+        l += len(lc)
+        # print(len(lc.fill_gaps(method='gaussian_noise')))
+        print(len(lc.fill_gaps(method='NaN')))
+        print(len(lc.fill_gaps(method='zero')))
+    assert l == lc_length
+    assert len(lc.fast_bin(10)) == 10
+
+    # test bin LC with number of bins larger than the length of LC
+    assert len(lc.fast_bin(lc_length + 10)) == lc_length + 10
