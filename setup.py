a2c7f5b6a5cd8a84c6f317d54b4031e5b0fb54d8
Kaiming Cui <ckm@nao.cas.cn>
2023-11-09 13:45:50 +0800

    Preparation for uploading

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
         # LightKurve-ext
         This is an extension for the [LightKurve](https://github.com/lightkurve/lightkurve) package.
         It provides some useful functions for light curve analysis.
-        E.g. ``search_local_lightcurve``.
+        E.g. ``search_lightcurve``.
     """,
     long_description_content_type="text/markdown",
     package_dir={"": "src"},
